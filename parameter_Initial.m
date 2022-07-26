93716f225e54902fee19f7ed93ceda25a4dcfd92
kali_mufasa <kalihao569@gmail.com>
2022-07-26 14:45:48 +0800

    初始化

---
+++ parameter_Initial.m
@@ -0,0 +1,10 @@
+%运行simulink文件前请先运行此参数初始化程序
+
+clear;clc;close all;
+ts = 0.01;
+wn = 5;zeta = 0.1;
+td = pi / (wn*sqrt(1-zeta^2));
+K = exp((-zeta*pi)/(sqrt(1-zeta^2)));
+%第一个幅值A1 第二个A2
+A1 = 1/(1+K);
+A2 = K/(1+K);
